0abadea96a1436e5ffb54d4de400bbb1b150b1ac
walid <walidsher@hotmail.com>
2022-04-24 18:22:04 +0200

    update added more hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,5 +3,7 @@ public class HelloTest {
 
         System.out.println("HelloTest");
         System.out.println("Devloping hello feature");
+        System.out.println("local  repository iis already publiced to gituhb");
+        System.out.println("Doing more work");
     }
 }
